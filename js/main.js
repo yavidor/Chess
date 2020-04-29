d27d70546695928d228ec77743d83044bd89fe29
yavidor <yonatan@avidor.org>
2020-04-29 23:32:40 +0300

    Last changes before I sleep

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ ctx.fillStyle = "rgb(255,0,0)";
 ctx.fillRect(0,0,can.height,can.width);
 ctx.fill();
 console.log(grid);
+for (let i=0;i<grid.length;i++)
+  for (let j=0;j<grid.length;j++)
+    grid[i][j] = new spot(i+1,j+1);
--- js/spot.js
+++ js/spot.js
@@ -2,4 +2,5 @@ function spot(i,j) {
   this.i = i;
   this.j = j;
   this.piece = "none";
+  this.i+this.j%2==0?this.color="white":this.color="brown";
 }
